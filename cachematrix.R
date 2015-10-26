0448dcd6b3f844f45f4ca2cbe250ef0c51156934
zeamxie <xieshaojun0621@qq.com>
2015-10-25 23:47:11 -0400

    write function
    finish the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invMat <- NULL;
+    setMat <- function(y){
+        x <<- y;
+        invMat <<- NULL;
+    }
+    
+    getMat <- function() {x}
+    setInverseMat <- function(inverseMat) invMat <<- inverseMat;
+    getInverseMat <- function() invMat;
+    list(setMat=setMat, getMat=getMat, setInverseMat=setInverseMat, getInverseMat=getInverseMat)
 }
 
 
 ## Write a short comment describing this function
-
+# Use solve function to get the inverse of the matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     temInv <- x$getInverseMat();
+     
+     if(!is.null(temInv)) {
+        message("getting cached data.")
+        return(temInv);
+     }
+    mat <- x$getMat()
+    invMat <- solve(mat)
+    x$setInverseMat(invMat)
+    x$getInverseMat()
 }
